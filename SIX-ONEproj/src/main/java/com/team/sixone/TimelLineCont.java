9c8a35decb5d8b7e2eaa25a5a5504fbb24f58366
subin129 <subin129@naver.com>
2020-05-29 16:13:36 +0900

    글 작성폼(모달) 작성

--- SIX-ONEproj/src/main/java/com/team/sixone/TimelLineCont.java
+++ SIX-ONEproj/src/main/java/com/team/sixone/TimelLineCont.java
@@ -1,28 +1,51 @@
 package com.team.sixone;
 
-import java.text.DateFormat;
-import java.util.Date;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Locale;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
+import javax.sql.DataSource;
 
-import org.apache.tiles.request.Request;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+
 @Controller
 public class TimelLineCont {
+	private Connection conn;
+	private PreparedStatement psmt;
+	private ResultSet rs;
+	private ServletContext context;
+	
+	
 	@RequestMapping(value = "/TimeLine.do", method = RequestMethod.GET)
-	public String home(Locale locale, Model model, HttpServletRequest req) {
+	public String TimeLine(Locale locale, Model model, HttpServletRequest req) {
+		
+		
+		DAO dao = new DAO(req.getSession().getServletContext());
+		
 		
 		model.addAttribute("aaa", "resources/images/black.jpg");
 		model.addAttribute("bbb", "resources/images/classes-1.jpg");
-		String[] images = new String[10];
-		for(int i=0;i<10;i++)
-			images[i] = "resources/images/classes-"+(i+1)+".jpg";
+		String[] images = dao.test();
 		
 		req.setAttribute("images", images);
+		System.out.println("기존2");
+		
+		///======================================
+		
+		
+
+		
+		
 		
 		return "/TimeLine.tiles";
 	}
@@ -34,5 +57,14 @@ public class TimelLineCont {
 		
 		return "/exercise.tiles";
 	}
+	
+	@RequestMapping(value = "/blog.do", method = RequestMethod.GET)
+	public String blog(Locale locale, Model model) {
+	
+		
+		
+		return "/blog.tiles";
+	}
+
 
 }
--- SIX-ONEproj/src/main/webapp/WEB-INF/template/Bottom.jsp
+++ SIX-ONEproj/src/main/webapp/WEB-INF/template/Bottom.jsp
@@ -92,7 +92,6 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
 	
 		<!-- 로그인창 스크립트 -->
 <script src="<c:url value='/resources/LoginCSS/vendor/animsition/js/animsition.min.js'/>"></script>
-<script src="<c:url value='/resources/LoginCSS/vendor/bootstrap/js/popper.js'/>"></script>
 <script src="<c:url value='/resources/LoginCSS/vendor/select2/select2.min.js'/>"></script>
 <script src="<c:url value='/resources/LoginCSS/vendor/daterangepicker/moment.min.js'/>" ></script>
 <script src="<c:url value='/resources/LoginCSS/vendor/daterangepicker/daterangepicker.js'/>"></script>
@@ -101,7 +100,7 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
 <!-- 로그인창 스크립트 -->
 
 
-
+<script src="<c:url value='/resources/LoginCSS/vendor/bootstrap/js/popper.js'/>"></script>
 
 
 	</body>
